83e365bde0246b80354aaf4465f56300d4559241
place0 <thdud030101@naver.com>
2022-08-14 17:59:08 +0900

    로그인 수정

--- mainproj/accounts/views.py
+++ mainproj/accounts/views.py
@@ -4,35 +4,26 @@ from django.contrib import auth
 
 def signup(request):
     if request.method == "POST":
-        if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(
-                username=request.POST['username'],
-                password=request.POST['password1']
-            )
-            auth.login(request, user)
+        if request.POST.get('PASSWORD')==request.POST.get('Confirm PW'):
+            user=User.objects.create_user(request.POST.get('username'), password=request.POST.get('PASSWORD'))
+            auth.login(request,user)
 
-            return render(request, "login.html")
-
-    return render(request, "signup.html")
+            return redirect('login')
+    return render(request,'signup.html')
 
 def login(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = auth.authenticate(
-            request, username=username, password=password
-        )
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('PASSWORD', None)
+        user = auth.authenticate(request, username=username, password=password)
 
         if user is not None:
             auth.login(request, user)
             return redirect('home')
         else:
-            return render(request, "login.html", {
-                'error': 'Username or Password is incorrect.',
-            })
+            return render(request, 'home.html')
     else:
-        return render(request, "login.html")
+        return render(request, 'login.html')
     
 def home(request):
     return render(request, 'home.html')
